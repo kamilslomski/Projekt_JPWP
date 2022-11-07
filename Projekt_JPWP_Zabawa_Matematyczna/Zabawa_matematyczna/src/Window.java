cc1b091986a6472e1083d154907efd3bcf01674d
--replace-all <--replace-all>
2022-11-07 11:55:41 +0100

    #2

--- Projekt_JPWP_Zabawa_Matematyczna/Zabawa_matematyczna/src/Window.java
+++ Projekt_JPWP_Zabawa_Matematyczna/Zabawa_matematyczna/src/Window.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends  JFrame {
-    JPanel gora;
+    JPanel top;
     JPanel panel;
     JPanel przyciski;
+    JPanel bottom;
+    JPanel answer;
+    JPanel time;
 
      Window(String nazwa) {
         super(nazwa);
@@ -12,19 +15,20 @@ public class Window extends  JFrame {
         setSize(1280, 1024);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        gora = new JPanel();
-        gora.setLayout(new BorderLayout());
-        gora.setPreferredSize(new Dimension(1280, 224));
-        gora.setBackground(Color.LIGHT_GRAY);
+        top = new JPanel();
+        top.setLayout(new BorderLayout());
+        top.setPreferredSize(new Dimension(1280, 200));
+        top.setBackground(Color.LIGHT_GRAY);
 
         panel = new JPanel();
-        panel.setBackground(Color.BLACK);
+        panel.setLayout(new BorderLayout());
+        panel.setBackground(Color.WHITE);
 
-        przyciski = new JPanel();
-        przyciski.setLayout(new GridLayout(1, 2));
+        bottom = new JPanel();
+        bottom.setLayout(new BorderLayout());
+        bottom.setPreferredSize(new Dimension(1280, 80));
 
         JTextField textField = new JTextField();
-        JTextArea textArea = new JTextArea();
         textField.setHorizontalAlignment(SwingConstants.CENTER);
 
         JButton buttonA = new JButton();
@@ -32,19 +36,49 @@ public class Window extends  JFrame {
         JButton buttonC = new JButton();
         JButton buttonD = new JButton();
 
-        JButton exit = new JButton();
-        JButton easy = new JButton();
-        JButton normal = new JButton();
-        JButton hard = new JButton();
+        JButton exit = new JButton("EXIT");
+        JButton easy = new JButton("EASY");
+        JButton normal = new JButton("NORMAL");
+        JButton hard = new JButton("HARD");
 
         JLabel answer_labelA = new JLabel();
         JLabel answer_labelB = new JLabel();
         JLabel answer_labelC = new JLabel();
         JLabel answer_labelD = new JLabel();
-        JLabel time_label = new JLabel();
-        JLabel result = new JLabel();
+        JLabel time_label = new JLabel("10");
+        JLabel result = new JLabel("3");
+        JLabel question = new JLabel("8*4=...");
+
+        przyciski = new JPanel();
+        przyciski.setLayout(new GridLayout(1, 4));
+        przyciski.add(easy);
+        przyciski.add(normal);
+        przyciski.add(hard);
+        przyciski.add(exit);
+
+        time = new JPanel();
+        time.setLayout(new GridLayout(1, 3));
+        time.add(time_label, FlowLayout.LEFT);
+        time.add(question, FlowLayout.CENTER);
+        time.add(result, FlowLayout.RIGHT);
 
 
+        answer = new JPanel();
+        answer.setLayout(new GridLayout(2, 2));
+        answer.add(answer_labelA);
+        answer.add(answer_labelB);
+        answer.add(answer_labelC);
+        answer.add(answer_labelD);
+
+        top.add(time, BorderLayout.CENTER);
+
+        bottom.add(przyciski, BorderLayout.CENTER);
+
+        add(top, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+        add(bottom, BorderLayout.SOUTH);
+
+        Body body = new Body();
 
     }
 }
